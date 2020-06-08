392470cd714dbb592f5e06042905558526cda881
snh3003 <shahsama542@gmail.com>
2020-06-08 12:28:12 +0530

    Updates

---
+++ precache-manifest.7e83d93f4bedf8e6feb9d3bac19a16d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc189bcc31279ac723c9996e0f42e347",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "0670fefbfb659515ae96",
+    "url": "/memegenerator/static/css/main.bb150c18.chunk.css"
+  },
+  {
+    "revision": "fde13f254540b63cfb82",
+    "url": "/memegenerator/static/js/2.7e352244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegenerator/static/js/2.7e352244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0670fefbfb659515ae96",
+    "url": "/memegenerator/static/js/main.93cce956.chunk.js"
+  },
+  {
+    "revision": "d772c24e0a973e433a65",
+    "url": "/memegenerator/static/js/runtime-main.fe740041.js"
+  }
+]);
